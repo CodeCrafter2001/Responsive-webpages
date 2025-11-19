4d2c7b38593797d660f10062edb6980bb891d4bf
Anmol Bansiwal <anmolbansiwal1@gmail.com>
2025-11-19 23:19:43 +0530

    done and dusted

--- responsive-clothing-website-main/responsive-clothing-website-main/assets/js/main.js
+++ responsive-clothing-website-main/responsive-clothing-website-main/assets/js/main.js
@@ -1,4 +1,4 @@
-/*=============== SHOW MENU ===============*/
+ /*=============== SHOW MENU ===============*/
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav__menu'),
       navToggle = document.getElementById('nav__toggle'),
@@ -29,17 +29,41 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 let swiperHome = new Swiper('.home__swiper', {
   loop: true,
   grabCursor: true,
-  slidesPreView: 'auto',
-   
+   slidesPerView: 'auto',
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakPoints:{
+    768:{
+        slidesPerView: 3,
+        centeredSlides: 'auto',
+    },
+    1152:{
+        centeredSlides: 'auto',
+        spaceBetween: -64, 
+    }
+  }
 })
  
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const bgHeader = ()=>{
+    const header= document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                        : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr= ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+})
+sr.reveal(`.home__swiper, .home__footer`)
+sr.reveal(`.home__circle`, {scale: 1.5, delay: 300})
+sr.reveal(`.home__subcircle`, {scale: 1.5, delay: 500})
+sr.reveal(`.home__title`, {scale: 1, origin: 'bottonm', delay: 1200})
+sr.reveal(`.swiper-button-prev, .swiper-button-next`, { origin: 'bottonm'})
